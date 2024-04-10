4f8d226f2a962f063977bfb4768dd08ab4b1297e
Rima Das <rimadas.automationtester@gmail.com>
2024-04-09 23:00:15 -0700

    Adding Gestures

--- src/main/java/utilities/AndroidGestures.java
+++ src/main/java/utilities/AndroidGestures.java
@@ -1,6 +1,12 @@
 package utilities;
 
+import org.openqa.selenium.DeviceRotation;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+
 import com.basePage.android.BasePage;
+import com.google.common.collect.ImmutableMap;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
@@ -17,7 +23,31 @@ public class AndroidGestures extends BasePage{
 		
 	}
 	
+	public void longPressGesture(WebElement element) {
+		
+		((JavascriptExecutor)driver).executeScript("mobile: longClickGuesture", ImmutableMap.of("elementId", ((RemoteWebElement) element).getId()));
+
+	}
 	
+	public void doubleClickGesture(WebElement element) {
+		
+		((JavascriptExecutor)driver).executeScript("mobile:doubleClickGesture", ImmutableMap.of("elementId",((RemoteWebElement) element).getId()));
+		
+	}
+	
+	public void swipeGesture(WebElement element) {
+		((JavascriptExecutor)driver).executeScript("mobile:swipeGesture", ImmutableMap.of(
+				"left", 100, "top", 100, "width", 200, "height", 200,
+				"direction", "left",
+				"percent", 0.75
+				));
+		
+	}
+	
+	public void rotateToLandscape() {
+		DeviceRotation landscape = new DeviceRotation(0, 0 , 90);
+		driver.rotate(landscape);
+	}
 	
 
 }
